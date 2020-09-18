dc0c8e3d69d000fb908ee24606154eeaf744043d
gabrielabdul <gabrielabdul1@gmail.com>
2020-09-18 19:13:50 +0000

    need to update each row

--- main.js
+++ main.js
@@ -2,16 +2,26 @@
 
 // print main prompt
 // enter while loop
-var board = require('./mastermind.js');
-while (i < 10) {
-    board.printBoard();
-    board.printPrompt();
-    //wait for input
-    r = board.checkInput();
-    if (r === 1){ 
-        printWinner();
-        break;}
-    board.printCheckerResults();
-    i++;
+var Board = require('./master.js').Board;
+let board = new Board();
+let row = 1;
+while (row <= 10) {
+	board.printBoard();
+	console.log(board.mastermind);
+    // wait for input
+    let code = board.get_guess();
+	console.log(code);
+	// update board with guess
+	board.updateRow(code, row);
+	board.printBoard();
+	// check input code agains mastermind
+//    r = board.checkInput();
+
+//    if (r === 1){
+  //      printWinner();
+  //      break;}
+  //  board.printCheckerResults();
+    row += 1;
+	console.log(row);
 }
-printLoser();
+// printLoser();
